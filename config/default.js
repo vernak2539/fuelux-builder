bffe62a185f15f26a4578585a89b0b76c6ee8692
Alex Vernacchia <avernacchia@exacttarget.com>
2014-11-04 11:27:18 +0000

    express: update to v4 in server file

--- config/default.js
+++ config/default.js
@@ -1,13 +1,7 @@
 module.exports = {
 	port: 4000,
 	ui: {
-		publicDir: '/public-optimized',
-		staticBase: '/'
-	},
-	mongo: {
-		useVCAP: true,
-		host: '127.0.0.1',
-		port: 27017,
-		dbname: 'test-build-for-files-db' // this db will be created if it's not there already. Good thing? Depends on what you're doing
+		publicDir: '/public-optimized'
+		, bowerBase: '/bower_components'
 	}
 };
