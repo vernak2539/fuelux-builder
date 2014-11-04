647d237128ec07724b815d9b318883a59f6521ca
Alex Vernacchia <avernacchia@exacttarget.com>
2014-11-04 12:59:37 +0000

    express: adding bower, updating index rendering

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,7 @@ var express = require( 'express' );
 var router = express.Router();
 
 router.get( '/', function( req, res ) {
-	res.render( 'index', { title: 'test' } );
+	res.render( 'index', { title: 'Fuel UX Builder' } );
 });
 
 module.exports = router;
-
-// var config = require( 'config' );
-// var _      = require( 'underscore' );
-//
-// var defaultOptions = {
-// 	title: 'Fuel UX Builder',
-// 	ui: JSON.stringify( config.ui ),
-// 	index: '{{$index+1}}',
-// 	stepDesc: '{{step.desc}}'
-// };
-//
-// module.exports = function( server ) {
-// 	server.get( '/', function( req, res ) {
-// 		res.render( 'index', _.extend( defaultOptions, { csrfToken: req.csrfToken() } ) );
-// 	});
-// };
